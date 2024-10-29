ef19d6503dfe7de4e6246f91d2ef562e387b6c48
theokunto <theofilus.business@gmail.com>
2024-10-29 15:42:09 +0700

    fix static route and content login

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const studentRouters = require('./src/routes/student.routes')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
+app.use(express.static(__dirname+'/public'));
 app.use('/', webRouters);
 app.use('/api/mahasiswa', studentRouters);
 app.listen(process.env.PORT, () => {
--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+
 router.get('/',function (req, res) {
-    res.sendFile(path.join(__dirname,'../view/login.html' ))
+    res.sendFile(path.join(__dirname,'../../public/login.html' ))
 })
 router.get('/dashboard',function (req, res) {
-    res.sendFile(path.join(__dirname,'../view/dashboard.html' ))
+    res.sendFile(path.join(__dirname,'../../public/dashboard.html' ))
 })
 router.get('/student',function (req, res) {
-    res.sendFile(path.join(__dirname,'../view/student.html' ))
+    res.sendFile(path.join(__dirname,'../../public/student.html' ))
 })
 module.exports=router
